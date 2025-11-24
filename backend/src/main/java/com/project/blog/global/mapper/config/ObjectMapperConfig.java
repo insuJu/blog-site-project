a2d39b5a3b5467ab715ba5da10d9f486b6681faa
insuJu <insubono_01@naver.com>
2025-11-24 15:28:23 +0900

    refactor(backend): LocalDateTime의 역/직렬화를 위한 registerModule 설정

--- backend/src/main/java/com/project/blog/global/mapper/config/ObjectMapperConfig.java
+++ backend/src/main/java/com/project/blog/global/mapper/config/ObjectMapperConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
     ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 형식 사용
         return objectMapper;
     }
