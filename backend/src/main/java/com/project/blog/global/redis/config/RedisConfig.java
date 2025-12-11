9cf3978784e8ed740ef96588ac8260c0119146e6
insuJu <insubono_01@naver.com>
2025-12-11 22:00:49 +0900

    chore(backend): Redis 인프라 설정 추가
      - Spring Data Redis 의존성 추가
      - Redis 연결 설정
      - RedisTemplate 빈 설정 (String 직렬화)
      - Lettuce 커넥션 팩토리 구성

---
+++ backend/src/main/java/com/project/blog/global/redis/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.project.blog.global.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
