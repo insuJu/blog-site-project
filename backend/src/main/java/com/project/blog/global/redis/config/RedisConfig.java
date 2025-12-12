9f752595abe276b7135971ec7c638ea7bf713c21
insuJu <insubono_01@naver.com>
2025-12-12 17:13:58 +0900

    fix(backend): -parameters 및 @Primary을 사용하여 주입 오류 해결

--- backend/src/main/java/com/project/blog/global/redis/config/RedisConfig.java
+++ backend/src/main/java/com/project/blog/global/redis/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.project.blog.global.redis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -48,6 +49,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
